b725da2b0df610c150188119fc262ca1f7d6433e
Gordon Nongkynrih <gordon@dolphinze.com>
2024-05-13 15:04:31 +0530

    auth node mull

--- config/mail.php
+++ config/mail.php
@@ -47,7 +47,7 @@ return [
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
-            'auth_mode'  => null,
+            // 'auth_mode'  => null,
             'verify_peer'  => false,
         ],
 
