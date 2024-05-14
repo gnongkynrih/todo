10d86a5a85f9235eaca3e4d2195d4ba98c778185
Gordon Nongkynrih <gordon@dolphinze.com>
2024-05-14 22:00:47 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 // Route::view('/', 'welcome');
 
-Route::view('/', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('/', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
+Route::middleware(['auth', 'verified'])
+    ->group(function () {
+        Route::controller(DashboardController::class)->group(function () {
+            Route::get('/', 'index')->name('dashboard');
+        });
+    });
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
